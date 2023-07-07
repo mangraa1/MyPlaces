793fb5288f847814e35884c3708454a8805cd068
mangraa1 <georgiu.spare@gmail.com>
2023-07-07 14:45:11 +0300

    adding TableVC, minor cell adjustment
    initial setting of cells, adding restaurants with photos

---
+++ MyPlaces/MainViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MainViewController.swift
+//  MyPlaces
+//
+//  Created by mac on 07.07.2023.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    let restaurantNames = [
+        "Royal Pizza", "Trattoria", "Osama",
+        "Passaj", "Balu", "Montana"
+    ]
+
+    //MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        var content = cell.defaultContentConfiguration()
+        content.text = restaurantNames[indexPath.row]
+        content.image = UIImage(named: restaurantNames[indexPath.row])
+
+        let imageSize = CGSize(width: 80, height: 80)
+        content.imageProperties.maximumSize = imageSize
+        content.imageProperties.reservedLayoutSize = imageSize
+
+        cell.contentConfiguration = content
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
