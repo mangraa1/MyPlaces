b8d085153062a73b58e23d9440dc92470ed97382
mangraa1 <georgiu.spare@gmail.com>
2023-07-24 17:29:46 +0300

    bug fixes
    fixed application crash when user saves new location and tries to login to MapVC without first setting values for new location

--- MyPlaces/Controllers/NewPlaceViewController.swift
+++ MyPlaces/Controllers/NewPlaceViewController.swift
@@ -79,26 +79,22 @@ class NewPlaceViewController: UITableViewController {
     //MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier != "showMap" {
-            return
-        }
+        if segue.identifier != "showMap" { return }
 
         let mapVC = segue.destination as! MapViewController
 
-        mapVC.place = currentPlace
+        mapVC.place.name = placeName.text!
+        mapVC.place.location = placeLocation.text
+        mapVC.place.type = placeType.text
+        mapVC.place.imageData = placeImage.image?.pngData()
     }
 
     //MARK: - Save place
 
     func savePlace() {
-        var image: UIImage?
 
         // Setting a default photo in case the user does not select his photo
-        if imageIsChanged {
-            image = placeImage.image
-        } else {
-            image = UIImage(named: "defaultImagePlaceholder")
-        }
+        let image = imageIsChanged ? placeImage.image : UIImage(named: "defaultImagePlaceholder")
 
         let imageData = image?.pngData()
 
